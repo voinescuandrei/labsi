5a23d47cd2e560f8592861452488006ae696ac22
Dan Dragomir <dragomir.dan@gmail.com>
2013-10-14 14:32:18 +0300

    Removes config scripts.

--- install-kernel.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-yum install tftp tftp-server
-cp tftp.config /etc/xinetd.d/tftp
-chmod 644 /etc/xinetd.d/tftp
-chmod u+s /sbin/ifconfig
-sed -i s/SELINUX=enforcing/SELINUX=disabled/g /etc/selinux/config
